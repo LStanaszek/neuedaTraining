80aeeacdd76ea93732b8ec6d01f4f4c847f69794
User111 <test@gmail.com>
2024-08-22 14:50:45 +0000

    Updated /historical/:ticker to user chart module for short intervals

--- src/routes/BrowseAPI.js
+++ src/routes/BrowseAPI.js
@@ -4,28 +4,39 @@ const yahooFinance = require('yahoo-finance2').default;
 
 const router = express.Router();
 
-// Individual routes
+/***************************************** Browse endpoints ********************************/
+
+//GET historical data given stock ticker and invertal of closing price for intervals 1m, 60m,.. 1mo
 router.get('/historical/:ticker', async (req, res) => {
     const { ticker } = req.params;
-    const { interval = '1d', start = '2023-01-01', end = '2023-12-31' } = req.query;
+    const { interval = '1d', start, end } = req.query;
 
     try {
-        // Convert start and end dates to UNIX timestamps
-        const period1 = new Date(start).getTime() / 1000;
-        const period2 = new Date(end).getTime() / 1000;
+        let data;
+        
+        const intradayIntervals = ['1m', '2m', '5m', '15m', '30m', '60m'];
+        if (intradayIntervals.includes(interval)) {
+            // Use the 'chart' module if intraday intervals
+            data = await yahooFinance.chart(ticker, {
+                interval,
+                period1: start,
+                period2: end,
+            });
+        } else {
+            // Use the 'historical' module if interval larger than a day
+            data = await yahooFinance.historical(ticker, {
+                period1: start,
+                period2: end,
+                interval,
+            });
+        }
 
-        const historical = await yahooFinance.historical(ticker, {
-            interval, // '1m', '5m', '15m', '1d', '1wk', '1mo'
-            period1,
-            period2,
-        });
-
-        console.log(historical);
-        res.json(historical);
+        res.json(data);
     } catch (error) {
-        console.error('Error fetching historical data:', error);
-        res.status(500).json({ error: 'An error occurred while fetching historical data.' });
+        console.error('Error fetching data:', error);
+        res.status(500).json({ error: 'An error occurred while fetching data.' });
     }
 });
+
 
 module.exports = router;
