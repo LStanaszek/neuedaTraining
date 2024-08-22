7e79f154c56a5c7e5960ceb385138b04e66eb577
Lukasz Stanaszek <lstan2104@gmail.com>
2024-08-22 08:45:04 +0000

    connected database to backend

--- src/app.js
+++ src/app.js
@@ -1,14 +1,38 @@
 const express = require('express');
-require("dotenv").config();
+//require("dotenv").config();
 const axios = require("axios");
-const app = express();
-const PORT = 3000;
 const bodyParser = require("body-parser");
-const {Sequelize, DataTypes} = require("sequelize");
+const {sequelize, Sector, Stock, Watchlist, User, Transaction} = require('./utils/createDB');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 //app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
+
+//const browseRoutes = require('./routes/sectorRoutes');
+
+// Use routes
+//app.use('/sectors', sectorRoutes);
+
+sequelize.authenticate()
+    .then(() => {
+        console.log('Connection established successfully.');
+    })
+    .catch((err) => {
+        console.error('Failed to connect:', err);
+    });
+
+    /* Test Query
+app.get('/', async (req, res) => {
+        try {
+          const watchlist = await Watchlist.findAll();
+          res.json(watchlist);
+        } catch (error) {
+          res.status(500).json({ error: 'An error occurred while fetching sectors.' });
+        }
+      });
+    */
 
 app.listen(PORT, () => {
     console.log(`Server Running on localhost//:${PORT}`)
---
+++ src/utils/createDB.js
@@ -0,0 +1,88 @@
+const {Sequelize, DataTypes} = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  }
+);
+
+const Sector = sequelize.define('Sector', {
+    sector_id: {type: DataTypes.INTEGER, autoIncrement: true, allowNull:false, primaryKey: true},
+    sector_name: {type: DataTypes.STRING(255), allowNull: false, unique: true},
+    }, {
+    tableName:'sectors',
+    timestamps:false
+  });
+
+const Stock = sequelize.define('Stock', {
+    stock_id : {type: DataTypes.INTEGER, autoIncrement:true, allowNull:false, primaryKey:true},
+    stock_name : {type: DataTypes.STRING, allowNull:false},
+    ticker : {type: DataTypes.STRING, allowNull:false, unique:true},
+    sector_id : {type: DataTypes.INTEGER, allowNull:false, references: {model: Sector, key: 'sector_id'}, onDelete: 'CASCADE'},
+    num_employees : {type: DataTypes.INTEGER, allowNull:false}
+    }, {
+    tableName:'stocks',
+    timestamps:false
+});
+
+Sector.hasMany(Stock, { foreignKey: 'sector_id' });
+Stock.belongsTo(Sector, { foreignKey: 'sector_id' });
+
+const Watchlist = sequelize.define('Watchlist', {
+    watch_id : {type: DataTypes.INTEGER, autoIncrement:true, allowNull:false, primaryKey:true},
+    stock_id : {type: DataTypes.INTEGER, allowNull:false, references: {model: Stock, key: 'stock_id'}, onDelete: 'CASCADE'},
+    }, {
+    tableName:'watchlist',
+    timestamps:false
+});
+
+Stock.hasMany(Watchlist, { foreignKey: 'stock_id' });
+Watchlist.belongsTo(Stock, { foreignKey: 'stock_id' });
+
+const User = sequelize.define('User', {
+    user_id : {type: DataTypes.INTEGER, autoIncrement:true, allowNull:false, primaryKey:true},
+    username : {type: DataTypes.STRING, allowNull:false},
+    balance : {type: DataTypes.DECIMAL(10,4), allowNull:false},
+    }, {
+    tableName:'users',
+    timestamps:false
+});
+
+const Transaction = sequelize.define('Transaction', {
+    trade_id: {type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true,},
+    trade_timestamp: {type: DataTypes.DATE, defaultValue: DataTypes.NOW, allowNull: false,},
+    stock_id: {type: DataTypes.INTEGER, allowNull: false, references: { model: Stock, key: 'stock_id',}, onDelete: 'CASCADE',},
+    share_quantity: {type: DataTypes.INTEGER, allowNull: false,},
+    stock_price: {type: DataTypes.DECIMAL(10, 4), allowNull: false,},
+    user_id: {type: DataTypes.INTEGER, allowNull: false, references: { model: User, key: 'user_id',}, onDelete: 'CASCADE', },
+  } , {
+    tableName:'transactions',
+    timestamps:false
+});
+  
+  Stock.hasMany(Transaction, { foreignKey: 'stock_id' });
+  User.hasMany(Transaction, { foreignKey: 'user_id' });
+  Transaction.belongsTo(Stock, { foreignKey: 'stock_id' });
+  Transaction.belongsTo(User, { foreignKey: 'user_id' });
+
+  sequelize.sync({ force: false })
+  .then(() => {
+    console.log('Database synchronized');
+  })
+  .catch((error) => {
+    console.error('Error synchronizing database:', error);
+  });
+
+module.exports = {
+  sequelize,
+  Sector,
+  Stock,
+  Watchlist,
+  User,
+  Transaction,
+};
