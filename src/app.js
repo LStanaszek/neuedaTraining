38cc894e6041cbfc00457f061eb2d2cf83c21956
Lukasz Stanaszek <lstan2104@gmail.com>
2024-08-29 08:57:06 +0000

    added first test

--- src/app.js
+++ src/app.js
@@ -1,38 +1,5 @@
-const express = require('express');
-require("dotenv").config();
-const cors = require('cors');
-const bodyParser = require("body-parser");
-const {sequelize, Sector, Stock, Watchlist, User, Transaction} = require('./utils/createDB');
-const app = express();
+const app = require('./index');
 const PORT = process.env.PORT || 3000;
-
-app.use(express.json());
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(cors());
-
-const stockInfo = require('./routes/StockInfoAPI');
-const search = require('./routes/StockInfoAPI');
-//const browseRoutes = require('./routes/sectorRoutes');
-const dashboardRoutes = require('./routes/DashboardAPI');
-const browseRoutes = require('./routes/BrowseAPI');
-const checkoutRoutes = require('./routes/CheckoutAPI');
-
-// Use routes
-//app.use('/sectors', sectorRoutes);
-app.use('/Dashboard', dashboardRoutes);
-app.use('/Browse', browseRoutes);
-app.use('/Stockinfo', stockInfo);
-app.use('/SearchSuggestion', search);
-app.use('/Checkout', checkoutRoutes);
-
-sequelize.authenticate()
-    .then(() => {
-        console.log('Connection established successfully.');
-    })
-    .catch((err) => {
-        console.error('Failed to connect:', err);
-    });
 
 app.listen(PORT, () => {
     console.log(`Server Running on localhost//:${PORT}`)
--- src/tests/stockInfoAPI.test.js
+++ src/tests/stockInfoAPI.test.js
@@ -1,12 +1,24 @@
 const request = require('supertest');
-const app = require('../app');
+const app = require('../index');
 
 describe('GET /StockInfo/Companyinfo?ticker=PG', () => {
     it('should return correct company info', async () => {
-      const response = await request(app).get('/Stockinfo/Companyinfo').query({ industry: 'PG' });
-      expect(response.body).toEqual([
-        { symbol: 'AAPL', industry: 'tech' },
-        { symbol: 'GOOG', industry: 'tech' }
-      ]);
+      const response = await request(app).get('/Stockinfo/Companyinfo').query({ ticker : 'PG' });
+      expect(response.body).toEqual({
+        "CompanyInfo": {
+            "country": "US",
+            "currency": "USD",
+            "exchange": "NEW YORK STOCK EXCHANGE, INC.",
+            "name": "Procter & Gamble Co",
+            "ticker": "PG",
+            "ipo": "1950-03-22T00:00:00.000Z",
+            "marketCapitalization": "397194.60",
+            "shareOutstanding": "2360.14",
+            "logo": "https://static2.finnhub.io/file/publicdatany/finnhubimage/stock_logo/PG.png",
+            "phone": "15139831100",
+            "weburl": "https://us.pg.com/",
+            "finnhubIndustry": "Consumer products"
+        }
+    });
     });
 });
